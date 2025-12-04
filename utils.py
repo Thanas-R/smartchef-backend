0d74de05b8bd4b809be77d76d014d0f465bfe2a9
Thanas R <thanas5.rd@gmail.com>
2025-12-04 22:01:01 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,1 @@
-from typing import List
 
-def calculate_matches(user_ingredients: List[str], all_recipes: List[dict]) -> List[dict]:
-    user_set = set(i.lower().strip() for i in user_ingredients)
-    matches = []
-    for recipe in all_recipes:
-        recipe_ingredients = [i.lower().strip() for i in recipe["ingredients"]]
-        has_ingredients = [i for i in recipe["ingredients"] if i.lower().strip() in user_set]
-        missing_ingredients = [i for i in recipe["ingredients"] if i.lower().strip() not in user_set]
-        match_percentage = round((len(has_ingredients) / len(recipe_ingredients)) * 100)
-        if match_percentage > 0:
-            matches.append({
-                **recipe,
-                "matchPercentage": match_percentage,
-                "hasIngredients": has_ingredients,
-                "missingIngredients": missing_ingredients
-            })
-    return sorted(matches, key=lambda x: x["matchPercentage"], reverse=True)
